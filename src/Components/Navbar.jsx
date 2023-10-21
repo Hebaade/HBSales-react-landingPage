3e449856c9c0690777f69a967f8063fcc559e996
Heba Adel <hebaadelali78@gmail.com>
2023-10-21 20:26:46 +0300

    first commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,11 +2,19 @@ import { Link } from "react-router-dom";
 
 
 export default function Navbar() {
+
+  
+  const Click = () => {
+    document.getElementById("ul").classList.toggle("display");
+  }
   return (
     <header>
-      <h2>HBSales</h2>
+      <div>
+        <h2>HBSales</h2>
+        <i className="fa-solid fa-bars-staggered" onClick={()=>Click()}></i>
+      </div>
       <nav>
-        <ul>
+        <ul id="ul" className="">
           <li>
             <Link to="/">Home</Link>
           </li>
